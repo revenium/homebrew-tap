6d961bfa791a27eb433b9d9b555a2f7ce2f36681
John D'Emic <john.demic@gmail.com>
2026-03-16 20:49:32 -0400

    Update revenium to 1.0.1

--- Formula/revenium.rb
+++ Formula/revenium.rb
@@ -1,28 +1,28 @@
 class Revenium < Formula
   desc "CLI for the Revenium AI Economic Control platform"
   homepage "https://github.com/revenium/revenium-cli"
-  version "1.0.2"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/revenium/revenium-cli/releases/download/v1.0.2/revenium-darwin-arm64.tar.gz"
-      sha256 "4595a5553d7dba6aff0149f0476dc378ab60bdffe0fc685d683e09ea243cc566"
+      url "https://github.com/revenium/revenium-cli/releases/download/v1.0.1/revenium-darwin-arm64.tar.gz"
+      sha256 "9a609761c7e459865570284f48b72596426ddd1f6f52a9e6fd1b520ae7a00487"
     end
     on_intel do
-      url "https://github.com/revenium/revenium-cli/releases/download/v1.0.2/revenium-darwin-amd64.tar.gz"
-      sha256 "b7332ae965e45b8e334f8f2276be50730efd8a0e7c364423928fe0244025b3ca"
+      url "https://github.com/revenium/revenium-cli/releases/download/v1.0.1/revenium-darwin-amd64.tar.gz"
+      sha256 "941baff8fa1223bb1ad9c83b71de1642bd4236c63ab0452ff2eb6eb737d8d24d"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/revenium/revenium-cli/releases/download/v1.0.2/revenium-linux-amd64.tar.gz"
-      sha256 "7dc407adb6b0bbe3519aa0d07dd5ba76c46c9dd9154ee346af259a52c5f6a78b"
+      url "https://github.com/revenium/revenium-cli/releases/download/v1.0.1/revenium-linux-amd64.tar.gz"
+      sha256 "97823819f14ee91ce5e939e2c967d753a1212ed17353dc79637c389f23a7a09c"
     end
     on_arm do
-      url "https://github.com/revenium/revenium-cli/releases/download/v1.0.2/revenium-linux-arm64.tar.gz"
-      sha256 "832c4ab8e5fb6a4d5cefda59f24983683b05d731c17d6e31ebfeb0db8ab98de2"
+      url "https://github.com/revenium/revenium-cli/releases/download/v1.0.1/revenium-linux-arm64.tar.gz"
+      sha256 "45c52c9aeeb2f67b71fc79bf79436aaedb1821f00034e34f2b568d76caf27b76"
     end
   end
 
@@ -31,6 +31,6 @@ class Revenium < Formula
   end
 
   test do
-    assert_match "1.0.2", shell_output("\#{bin}/revenium version")
+    assert_match "1.0.1", shell_output("\#{bin}/revenium version")
   end
 end
