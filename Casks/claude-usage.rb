d91490067987e0d09a6849562d4166c520e87e6f
Jason Cumberland <jason@revenium.io>
2026-08-10 21:28:55 +0000

    Update claude-usage to v3.3.8

--- Casks/claude-usage.rb
+++ Casks/claude-usage.rb
@@ -1,6 +1,6 @@
 cask "claude-usage" do
-  version "3.3.7"
-  sha256 "28b334ecbc956f38b8bcba1228ae17c16e5c6e3922caf1c6a5f8bd43e98a2b2b"
+  version "3.3.8"
+  sha256 "89d32fb3d0dddfa6baeb2ebdb97ab63a3e5e1618a0db4b5288f3ed589a17c92f"
 
   url "https://github.com/revenium/Claude-Usage-Tracker/releases/download/v#{version}/Claude-Usage.dmg"
   name "Claude Usage"
