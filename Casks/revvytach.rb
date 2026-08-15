b41555a2a87d3910bfe683ee80a06d476bc71967
Jason Cumberland <jason@revenium.io>
2026-08-15 03:37:51 +0000

    Update revvytach to v4.0.1

--- Casks/revvytach.rb
+++ Casks/revvytach.rb
@@ -1,6 +1,6 @@
 cask "revvytach" do
-  version "4.0.0"
-  sha256 "fa61e1c9439c262a07f0291eae6c1a1873f5f24f0708205830919090feca69cf"
+  version "4.0.1"
+  sha256 "ddeebe3ddb8219adbf905446a337d130879873f970fac0c0f3b199adebf16807"
 
   url "https://github.com/revenium/RevvyTach/releases/download/v#{version}/RevvyTach.dmg"
   name "RevvyTach"
@@ -13,7 +13,7 @@ cask "revvytach" do
   end
 
   auto_updates true
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "RevvyTach.app"
 end
