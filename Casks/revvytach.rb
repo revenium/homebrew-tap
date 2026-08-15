c2afcb8fbc592b54a068ebb186767d139365cf91
Jason Cumberland <jason@revenium.io>
2026-08-15 16:07:14 +0000

    Update revvytach to v4.0.2

--- Casks/revvytach.rb
+++ Casks/revvytach.rb
@@ -1,6 +1,6 @@
 cask "revvytach" do
-  version "4.0.1"
-  sha256 "ddeebe3ddb8219adbf905446a337d130879873f970fac0c0f3b199adebf16807"
+  version "4.0.2"
+  sha256 "476c6bab7ccff38d1dff75894916d216409a70f22d6a440407c1d5c6c627e7ca"
 
   url "https://github.com/revenium/RevvyTach/releases/download/v#{version}/RevvyTach.dmg"
   name "RevvyTach"
